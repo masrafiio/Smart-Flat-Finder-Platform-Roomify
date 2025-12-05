676e025a78baa1fa8832e34a1bf31f2bec11e485
masrafiio <masrafiislam505@gmail.com>
2025-12-05 18:23:41 +0600

    basic website created with login, register, logout and user profile isolation

---
+++ backend/src/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+// import adminRoutes from "./routes/adminRoutes.js";
+// import ownerRoutes from "./routes/ownerRoutes.js";
+// import customerRoutes from "./routes/customerRoutes.js";
+// import propertyRoutes from "./routes/propertyRoutes.js";
+// import bookingRoutes from "./routes/bookingRoutes.js";
+// import reviewRoutes from "./routes/reviewRoutes.js";
+// import forumRoutes from "./routes/forumRoutes.js";
+// import notificationRoutes from "./routes/notificationRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+// Middleware
+app.use(cors({origin: "http://localhost:5173",}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/authentication", authRoutes);
+// app.use("/api/admin", adminRoutes);
+// app.use("/api/owner", ownerRoutes);
+// app.use("/api/customer", customerRoutes);
+// app.use("/api/property", propertyRoutes);
+// app.use("/api/booking", bookingRoutes);
+// app.use("/api/review", reviewRoutes);
+// app.use("/api/forum", forumRoutes);
+// app.use("/api/notification", notificationRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port:", PORT);
+  });
+});
